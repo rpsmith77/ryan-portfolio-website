e8a02fceaf57d4e39b477e94c231b198b70e4938
Ryan Smith <rpsmith77@gmail.com>
2023-01-13 13:37:48 -0500

    forward and back through skin tones

--- app/HandWave.tsx
+++ app/HandWave.tsx
@@ -4,14 +4,14 @@ import {useEffect, useState} from "react";
 
 export default function HandWave(){
     const [index, setIndex] = useState(0);
-    const hand = ['👋🏻','👋🏼','👋🏽','👋🏾','👋🏿'];
+    const hand = ['👋🏻','👋🏼','👋🏽','👋🏾','👋🏿','👋🏾','👋🏽','👋🏼'];
 
     useEffect(() => {
         const interval = setInterval(() => {
             setIndex(index => (index + 1) % hand.length);
         }, 1800);
         return () => clearInterval(interval);
-    }, []);
+    }, [hand.length]);
 
     return(
         <span className={styles.wave}>{hand[index]}</span>
