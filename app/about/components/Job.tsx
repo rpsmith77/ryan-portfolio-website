715abf16059c93abc903129e333dea6ad6024894
Ryan Smith <rpsmith77@gmail.com>
2023-01-18 17:38:11 -0500

    reformat

--- app/about/components/Job.tsx
+++ app/about/components/Job.tsx
@@ -8,7 +8,7 @@ export default function Job(props: any) {
             </div>
             <h3 className={styles.jobDate}>{props.date}</h3>
             <ul className={styles.jobDesc}>
-                {props.description.map((line: string, index:number) => <li key={index}>{line}</li>)}
+                {props.description.map((line: string, index: number) => <li key={index}>{line}</li>)}
             </ul>
         </div>
     );
